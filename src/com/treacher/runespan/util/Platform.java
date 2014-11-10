e42b9cefa33abb97145a467a0ca60aafae64a9f1
Michael Treacher <m.w.treacher@gmail.com>
2014-11-11 00:19:52 +1100

    Started creating the map for runespan mid level.

---
+++ src/com/treacher/runespan/util/Platform.java
@@ -0,0 +1,29 @@
+package com.treacher.runespan.util;
+
+/**
+ * Created by Michael Treacher
+ */
+
+public enum Platform {
+
+    MISSILE_PLATFORM(70489,new int[]{24221}),
+    VINE_PLATFORM(70490, new int[]{24214,24216,24220}),
+    COMET_PLATFORM(70493, new int[]{24224,24222,24223});
+
+    private int platformId;
+    private int[] platformRequirementRuneIds;
+
+    Platform(int platformId, int[] platformRequirementRuneIds) {
+        this.platformId = platformId;
+        this.platformRequirementRuneIds = platformRequirementRuneIds;
+    }
+
+    public int getPlatformId() {
+        return platformId;
+    }
+
+    public int[] getPlatformRequirementRuneIds(){
+        return platformRequirementRuneIds;
+    }
+
+}
